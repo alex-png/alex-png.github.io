dcd95a6115cff73842169035a9b46324a39e7a79
alex-png <alexrod102797@gmail.com>
2020-02-22 15:51:48 -0500

    video start

--- scrollToElement.js
+++ scrollToElement.js
@@ -1,21 +1,16 @@
 function scrollToElement() {
     const about = document.getElementById("About")
-    const projects = document.getElementById("Projects")
+    const projects = document.getElementById("projects-section")
     const contact = document.getElementById("Contact")
     const nav = document.getElementsByClassName("nav")[0]
 
     if (event.target.title === "Learn") {
         console.log(nav.offsetHeight)
-
-
         nav.scrollIntoView({ behavior: "smooth", block: "start"})
-
-
-
     } else if (event.target.name === "About") {
         about.scrollIntoView({ behavior: "smooth" })
-    } else if (event.target.name === "Projects") {
-        projects.scrollIntoView({ behavior: "smooth" })
+    } else if (event.target.name === "projects-section" ) {
+        projects.scrollIntoView({ behavior: "smooth", block: "start"})
     } else if (event.target.name === "Contact"){
         console.log(event.target.name)
         contact.scrollIntoView({behavior: "smooth", block: "start"})
