d9f01d9cf8220e8d90e181f4343eded309a6bb3d
alex-png <alexrod102797@gmail.com>
2020-02-27 14:07:20 -0500

    modal position

--- findMyBeerModal.js
+++ findMyBeerModal.js
@@ -1,25 +1,3 @@
-function scrollToElement() {
-    const about = document.getElementById("About")
-    const projects = document.getElementById("projects-section")
-    const contact = document.getElementById("Contact")
-    const nav = document.getElementsByClassName("nav")[0]
-
-    if (event.target.title === "Learn") {
-        console.log(nav.offsetHeight)
-        nav.scrollIntoView({ behavior: "smooth", block: "start" })
-    } else if (event.target.name === "About") {
-        about.scrollIntoView({ behavior: "smooth" })
-    } else if (event.target.name === "projects-section") {
-        projects.scrollIntoView({ behavior: "smooth", block: "start" })
-    } else if (event.target.name === "Contact") {
-        console.log(event.target.name)
-        contact.scrollIntoView({ behavior: "smooth", block: "start" })
-    }else{
-        debugger
-    }
-}
-
-
 const modal = `<!DOCTYPE html>
 <html class="no-js">
 
@@ -60,4 +38,3 @@ const modal = `<!DOCTYPE html>
 </body>
 
 </html>`
-
